af218e94900576b15e54b3785b21746204f54dfe
anuar1991aida <anuar1991aida@gmail.com>
2023-11-06 11:08:18 +0500

    wsgi 06112023

--- finance/wsgi.py
+++ finance/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for finance project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+from pathlib import Path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finance.settings')
+
+path_home = str(Path(__file__).parents[1])
+if path_home not in sys.path:
+    sys.path.append(path_home)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'budjet.settings')
 
 application = get_wsgi_application()
